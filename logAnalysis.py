31a06ecefc90fbe55256f5937d0d9b525512c3da
Chike Ezeh <ezeh.chike@gmail.com>
2017-09-19 06:06:57 -0500

    Framework to query the database and present results

--- logAnalysis.py
+++ logAnalysis.py
@@ -5,11 +5,25 @@ import psycopg2  # module that connects to the database
 connection = psycopg2.connect(database="news")
 # create a cursor object
 cursor = connection.cursor()
-# SQL query to be executed
-query = "SELECT author FROM articles LIMIT 5"
-cursor.execute(query)
-# fetch the result of the query
-results = cursor.fetchall()
-for result in results:
-    print(result)
+# SQL querys to be executed
+# The querys are put in a list
+# so multiple querys can be executed by the python code.
+query1 = "SELECT name FROM authors WHERE id =1"
+query2 = "SELECT title FROM articles WHERE author = 1 LIMIT 1"
+query3 = "SELECT status FROM log LIMIT 1"
+querys = [query1, query2, query3]
+results = []  # empty list to put the result for each query
+for query in querys:
+    #  execute each of the querys
+    cursor.execute(query)
+    # fetch the result of the query
+    results.append(cursor.fetchall())
+
+answers = ["Author with id = 1 is: ",
+           "First article by Author 1 is: ", "The first status code is: "]
+# use string format to print answers out.
+print("\n{}{}\n".format(answers[0], results[0][0][0]))
+print("{}{}\n".format(answers[1], results[1][0][0]))
+print("{}{}\n".format(answers[2], results[2][0][0]))
+
 connection.close()
